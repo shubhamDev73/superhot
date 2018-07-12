34833cd7154fc69ad3cdf28c7bc01add9e37225c
Shubham Gupta <shubham0209@gmail.com>
2018-07-12 18:41:32 +0530

    Variables and fire skeleton

--- Assets/_Scripts/Enemy.cs
+++ Assets/_Scripts/Enemy.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
 	public static Transform player;
+	private int timer;
 
 	void Start () {
+		timer = 0;
 		if(!player)
 			player = GameObject.Find("Player").transform;
 	}
@@ -14,5 +14,14 @@ public class Enemy : MonoBehaviour {
 	void FixedUpdate () {
 		transform.LookAt(player);
 		transform.Translate(0, 0, 0.1f, Space.Self);
+		timer++;
+		if(timer > 30){
+			Fire();
+			timer = 0;
+		}
+	}
+
+	void Fire () {
+		//fire at player
 	}
 }
--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -2,15 +2,29 @@
 
 public class Movement : MonoBehaviour {
 
+	private static float speed = 10;
+	private static float rotLimit = 20;
+
 	void Update () {
-		transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*10, 0, Input.GetAxis("Vertical")*Time.deltaTime*10, Space.Self);
+		//move and rotate
+		transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed, Space.Self);
 		transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
 		transform.GetChild(0).Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
 		Vector3 angles = transform.GetChild(0).localEulerAngles;
-		transform.GetChild(0).localEulerAngles = new Vector3(angles[0] > 180 ? Mathf.Clamp(angles[0], 290, 360) : Mathf.Clamp(angles[0], 0, 70), angles[1], angles[2]);
+		transform.GetChild(0).localEulerAngles = new Vector3(angles[0] > 180 ? Mathf.Clamp(angles[0], 270 + rotLimit, 360) : Mathf.Clamp(angles[0], 0, 90 - rotLimit), angles[1], angles[2]);
+
+		//managing time
 		if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
 			Time.timeScale = 1;
 		else
 			Time.timeScale = 0;
+
+		//firing
+		if(Input.GetButton("Fire"))
+			Fire();
+	}
+
+	void Fire () {
+		//fire
 	}
 }
