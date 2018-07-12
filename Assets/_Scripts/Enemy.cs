21a1317b4c2f97e8697aaa495815abfdce96d419
Shubham Gupta <shubham0209@gmail.com>
2018-07-12 18:31:21 +0530

    Added enemy script

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public static Transform player;
+
+	void Start () {
+		if(!player)
+			player = GameObject.Find("Player").transform;
+	}
+
+	void FixedUpdate () {
+		transform.LookAt(player);
+		transform.Translate(0, 0, 0.1f, Space.Self);
+	}
+}
